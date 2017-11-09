e633f3bebc22f3907b0500cdfa4500978fac3da1
Luis Fernando Alvarez <alvarez.lf@gmail.com>
2017-11-09 16:13:05 -0500

    Homework 01 - Without whoDidIt

--- homework02/src/LogAnalysis.hs
+++ homework02/src/LogAnalysis.hs
@@ -25,10 +25,15 @@ import Log
 -- Unknown "This is not in the right format"
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage message = let list = words message in
+   case list of
+     ("I":time:msg)       -> LogMessage Info (read time) (unwords msg)
+     ("W":time:msg)       -> LogMessage Warning (read time) (unwords msg)
+     ("E":level:time:msg) -> LogMessage (Error (read level)) (read time) (unwords msg)
+     _                    -> Unknown "This is not in the right format"
 
 parse :: String -> [LogMessage]
-parse = undefined
+parse = map parseMessage . lines
 
 ----------------------------------------------------------------------
 -- Exercise 2
@@ -40,7 +45,11 @@ parse = undefined
 --
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert msg@LogMessage{} Leaf = Node Leaf msg Leaf
+insert msg1@(LogMessage _ ts1 _) (Node leftTree msg2@(LogMessage _ ts2 _) rightTree)
+  | ts1 < ts2 = Node (insert msg1 leftTree) msg2 rightTree
+  | otherwise = Node leftTree msg2 (insert msg1 rightTree)
+insert _ tree = tree
 
 ----------------------------------------------------------------------
 -- Exercise 3
@@ -52,7 +61,7 @@ insert = undefined
 --
 
 build :: [LogMessage] -> MessageTree
-build = undefined
+build = foldr insert Leaf
 
 ----------------------------------------------------------------------
 -- Exercise 4
@@ -64,7 +73,8 @@ build = undefined
 --
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
 
 ----------------------------------------------------------------------
 -- Exercise 5
@@ -76,7 +86,11 @@ inOrder = undefined
 --
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong =  map getMessage . inOrder . build . filter isSevere
+  where isSevere (LogMessage (Error s) _ _) = s > 50
+        isSevere _                          = False
+        getMessage (LogMessage _ _ message) = message
+        getMessage _                        = ""
 
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
